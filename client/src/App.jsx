7216dd443ea9574e82a12e3e4a24258d538342e8
Will Otwell <willotwell07@icloud.com>
2023-06-28 15:43:09 -0600

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Navbar, Footer, Services, Transactions, Welcome } from './components';
+import { useState } from 'react'
+
+const App = () => {
+
+  return (
+    <div className="min-h-screen"> 
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome /> 
+      </div>
+      <Services /> 
+      <Transactions /> 
+      <Footer /> 
+    </div>
+  )
+}
+
+export default App
