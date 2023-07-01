ececd0608c080b9bcf3a690e1d1d12820a235313
Will Otwell <willotwell07@icloud.com>
2023-06-30 23:39:38 -0600

    Create Welcome Section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
-import logo from '../../images/logo.png';
+import logo from '../../images/Background.png';
 
 const NavbarItem = ({ title, classProps }) => {
     return (
@@ -18,7 +18,7 @@ const Navbar = () => {
     return (
         <nav className='w-full flex justify-between items-center p-4 md:justify-center'>
             <div className='flex-initial justify-center items-center md:flex-[0.5]'>
-                <img src={ logo } alt='logo' className='w-32 cursor-pointer'></img>
+                <img src={ logo } alt='logo' className='w-10 cursor-pointer'></img>
             </div>
             <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
                 {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
